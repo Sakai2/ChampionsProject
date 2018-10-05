0675fb5ea0263fae1346c496d71376a38a301af5
Sakai2 <tom.caginacci@epita.fr>
2018-10-05 22:36:54 +0200

    fix DetectBlocs

--- src/DetectBlocText.c
+++ src/DetectBlocText.c
@@ -4,37 +4,37 @@
 #include "Matrix.h"
 
 //colorise the pixels between two '1' verticaly
-void coloriseV(Matrix mat,int i, int j int count)
+void coloriseV(Matrix mat,int i, int* j int count)
 {
-  for(int k = 0; k < count; k++)
-  {
-      mat.mat[i+j*mat.y] = 1;
-  }
+	for (int k = 0; k < count; k++)
+	{
+		mat.mat[i + j * mat.y] = 1;
+	}
 }
 
 //colorise the pixels between two '1' honrizontaly
 void coloriseH(Matrix mat,int i, int j, int count)
 {
-  for(int k = 0; k < count; k++)
-  {
-      mat.mat[j+i*mat.y] = 1;
-  }
+	for(int k = 0; k < count; k++)
+	{	
+		mat.mat[j+i*mat.y] = 1;
+	}
 }
 
 //calcul the distance between two '1' in the image verticaly
 int distanceV(Matrix mat, int i, int j, int max, Matrix Colori)
 {
   int count = 0;
-  while(mat.mat[i+j*mat.y] == 0  && j < mat.x && count < max)
-  {
-      count++;
-	  j += 1;
-  }
+  while(mat.mat[i + j * mat.y] == 0  && j < mat.x && count < max)
+	{
+	  count++;
+	  j++;
+	}
   if(count <= max)
-  {
-     coloriseV(colori,i,j,count)
+	{
+     coloriseV(colori, i , j ,count)
 	 return count;
-  }
+	}
   else
     return -1;
 }
@@ -42,68 +42,74 @@ int distanceV(Matrix mat, int i, int j, int max, Matrix Colori)
 //calcul the distance between two '1' in the image honrizontally
 int distanceH(Matrix mat, int i, int j, int max, Matrix colori)
 {
-  int count = 0;
-  while (mat.mat[j + i * mat.y] == 0 && j < mat.y && count < max)
-  {
-	  count++;
-	  j += 1;
-  }
-  if(count <= max)
-  {
-      coloriseV(colori,i,j,count);
-      return count;
-  }
-  else
-    return -1;
+	int count = 0;
+	while (mat.mat[j + i * mat.y] == 0 && j < mat.y && count < max)
+	{
+		count++;
+		j += 1;
+	}
+
+	if (count <= max)
+	{
+		coloriseV(colori, i, j, count);
+		return count;
+	}
+
+	else
+	{
+		return -1;
+	}
 }
 
 //give us an image after a Vertical RLSA
 Matrix RlsaVertical(Matrix mat, int max)
 {
-  Matrix c = ConstructMatrix(mat.x, mat.y);
-  initMatrix(c);
-  f
-  for(int i = 0; i < c.y; i++)
-  {
-      for(int j = 0; j < c.x; j++)
-	  {
-			if(mat.mat[i+j*c.y] == 1)
+	Matrix c = ConstructMatrix(mat.x, mat.y);
+    initMatrix(c);
+	for (int i = 0; i < c.y; i++)
+	{
+		for (int j = 0; j < c.x; j++)
+		{
+			if (mat.mat[i + j * c.y] == 1)
 			{
-				int test = distanceV(mat,i,j,max,c);
-				if(test != 1)
-					j+=test;
+				int test = distanceV(mat, i, j, max, c);
+				if (test != -1)
+					j += test;
 			}
-	  }
-  }
-  return c;
+		}
+	}
+	return c;
 }
+
 
 //give us an image after a Honrizontal RLSA
 Matrix RlsaHorizontal(Matrix mat, int max)
 {
-  Matrix c = ConstructMatrix(mat.x, mat.y);
-  initMatrix(c);
-
-  for(int i = 0; i < c.x; i++)
-  {
-		for(int j = 0; j < c.y; j++)
+	Matrix c = ConstructMatrix(mat.x, mat.y);
+	initMatrix(c);
+	for (int i = 0; i < c.x; i++)
+	{
+		for (int j = 0; j < c.y; j++)
 		{
-			if(c.mat[j + i * c.y] == 1)
+			if (c.mat[j + i * c.y] == 1)
 			{
-				int test = distanceH(mat,i,j+1,max,c) 
+				int test = distanceH(mat, i, j + 1, max, c);
 				if (test != -1)
-				j+= test;
+				{
+					j += test;
+				}
 			}
 		}
-  }
-  return c;
+	}
+	return c;
 }
+
 
 //Main fonction of the LRSA, call all of the fonction 
 Matrix RlsaGlobal(Matrix mat, int max)
 {
   Matrix vertical = RlsaVetical(mat,max);
   Matrix Horizontal = RlsaHorizontal(mat,max);
-  Matrix RLSA = OrBinary(Vertical,Horizontal); //Or with two matrices for the 0
+  Matrix RLSA = OrBinary(Vertical,Horizontal); //"Or" for the 0 with two matrices
   return RLSA;
 }
