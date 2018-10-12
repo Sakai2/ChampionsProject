0d0466f0aacd5c7feb54271775e83b89726702cf
Sakai2 <tomcagninacci@hotmail.fr>
2018-10-12 15:46:46 +0200

    nn .h

--- src/neuralNetwork.h
+++ src/neuralNetwork.h
@@ -1,6 +1,21 @@
 #ifndef NeuralNetwork_h
 #define NeuralNetwork_h
 
+typedef struct RRN
+{
+        int nbrInput;
+        int nbrHidden;
+        int nbrOutput;
+
+        double *InputValues;
+        double *Goal;
+
+        double Error_rate;
+}RRN;
+
+double Sigmoide(double x);
+RRN Initialize();
+void InitializeValues(RRN *reu);
 
 
 #endif
