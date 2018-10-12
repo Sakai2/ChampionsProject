f2f978f555abd686bb2c77246c14b066e2ea8b7a
Matthieu Alvergnat <matthieu.alvergnat@epita.fr>
2018-10-12 13:48:34 +0200

    NeuralNetwork 0.0

--- src/neuralNetwork.c
+++ src/neuralNetwork.c
@@ -7,3 +7,50 @@ double Sigmoide(double x)
   return (1.0/(1.0 + exp(-x)));
 }
 
+double Random()
+{
+	return rand()/(double)RAND_MAX;
+}
+
+typedef struct RRN
+{
+	int nbrInput;
+	int nbrHidden;
+	int nbrOutput;
+
+	double *InputValues;
+	double *Goal;
+
+	double Error_rate;
+}RRN;
+
+RRN Initialize()
+{
+	RRN neu;
+	neu.nbrInput = 2;
+	//neu.Hidden = 3;
+	neu.InputValue = malloc(sizeof(double)*8);
+	neu.Goal = malloc(sizeof(double)*4);
+	
+	neu.Error_rate = 0.0;
+
+	return neu;
+}
+
+void InitializeValues(RRN *reu)//*neu 
+}
+*((*neu).InputValues[0]) = 0; // pointeur to pointeur
+*((*neu).InputValues[1]) = 0;
+*((*neu).InputValues[2]) = 1;
+*((*neu).InputValues[3]) = 0;
+*((*neu).InputValues[4]) = 0;
+*((*neu).InputValues[5]) = 1;
+*((*neu).InputValues[6]) = 1;
+*((*neu).InputValues[7]) = 1;
+
+*((*neu).Goal[0]) = 0;
+*((*neu).Goal[1]) = 1;
+*((*neu).Goal[2]) = 1;
+*((*neu).Goal[3]) = 0;
+
+}
